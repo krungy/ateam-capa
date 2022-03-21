25bad816d2d170d492cc972dba683ec762b031d1
krungy <furler711@gmail.com>
2022-03-21 18:48:43 +0900

    ✨ feat: domain 컴포넌트 index.ts 작성

--- src/components/domain/index.ts
+++ src/components/domain/index.ts
@@ -1,1 +1,3 @@
 export { default as DefaultContainer } from './DefaultContainer/DefaultContainer';
+export { default as ItemCard } from './ItemCard/ItemCard';
+export { default as Navigation } from './Navigation/Navigation';
