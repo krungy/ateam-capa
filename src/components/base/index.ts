9f03cc16665f9cb551cfb5e17589704756f52081
krungy <furler711@gmail.com>
2022-03-21 18:00:43 +0900

    ✨ feat: base 컴포넌트 index.ts 작성

---
+++ src/components/base/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './Button/Button';
+export { ArrowIcon, CheckIcon } from './Icon/Icon';
+export { default as Option } from './Option/Option';
+export { default as Toggle } from './Toggle/Toggle';
